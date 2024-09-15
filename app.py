e36b997cb74a77b63fdfb17d79a52a632196dd06
Ivan Lindgren <denis.kornilov12@yandex.ru>
2024-09-15 22:56:12 +0300

    ldap
    Ldap

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, render_template
 import pandas as pd
-import openpyxl
+from ldap3 import Server, Connection, ALL, MODIFY_REPLACE
 
 app = Flask(__name__)
+
+LDAP_SERVER = 'ldap://your_ldap_server'
+LDAP_USER = 'cn=admin,dc=example,dc=com'
+LDAP_PASSWORD = 'your_password'
+BASE_DN = 'dc=example,dc=com'
 
 
 @app.route('/')
@@ -13,7 +18,7 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return "Вы не загрузили файлы"
+        return "No file part"
 
     file = request.files['file']
 
@@ -22,9 +27,31 @@ def upload_file():
 
     if file:
         df = pd.read_excel(file)
-        return render_template('data.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+        for index, row in df.iterrows():
+            sync_user(row['номер студенческого'], row['ФИО студента'], row['баллы'])
 
-    return "Не удалось обработать файл"
+        return "Синхронизация аккаунтов прошла успешна..."
+
+    return "File processing error"
+
+
+def sync_user(student_id, full_name, points):
+    server = Server(LDAP_SERVER, get_info=ALL)
+    conn = Connection(server, LDAP_USER, LDAP_PASSWORD, auto_bind=True)
+
+    search_filter = f'(uid={student_id})'
+    conn.search(BASE_DN, search_filter, attributes=['uid', 'cn', 'points'])
+
+    if conn.entries:
+        # Если пользователь есть - обновим баллы:
+        dn = conn.entries[0].entry_dn
+        conn.modify(dn, {'points': [(MODIFY_REPLACE, [points])]})
+    else:
+        # Если пользователя нет - зарегистрируем и обновим банны:
+        dn = f'uid={student_id},{BASE_DN}'
+        conn.add(dn, ['inetOrgPerson'], {'uid': student_id, 'cn': full_name, 'points': points})
+
+    conn.unbind()
 
 
 if __name__ == '__main__':
