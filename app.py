9658f9955cc36b746d06a16e7cc11933dc469576
Ivan Lindgren <denis.kornilov12@yandex.ru>
2024-09-16 23:42:45 +0300

    Latex gloves make so hard

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from ldap3 import Server, Connection, ALL, MODIFY_REPLACE
 
 app = Flask(__name__)
 
-LDAP_SERVER = ''
-LDAP_USER = ''
-LDAP_PASSWORD = ''
-BASE_DN = ''
+LDAP_SERVER = 'ldap://localhost'
+LDAP_USER = 'cn=Manager,dc=my-domain,dc=com'
+LDAP_PASSWORD = 'secret'
+BASE_DN = 'dc=my-domain,dc=com'
 
 
 @app.route('/')
@@ -18,21 +18,22 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return "No file part"
+        return "Вы не загрузили файл"
 
     file = request.files['file']
 
     if file.filename == '':
-        return "No selected file"
+        return "Файл не выбран"
 
     if file:
         df = pd.read_excel(file)
         for index, row in df.iterrows():
-            sync_user(row['номер студенческого'], row['ФИО студента'], row['баллы'])
+            print(f"Обработка пользователя: {row[1]}, {row[2]}, {row[3]}")
+            sync_user(row[1], row[2], row[3])
 
-        return "Синхронизация аккаунтов прошла успешна..."
+        return "Аккаунты были загружены"
 
-    return "File processing error"
+    return "Ошибка при обработке файла"
 
 
 def sync_user(student_id, full_name, points):
@@ -40,16 +41,25 @@ def sync_user(student_id, full_name, points):
     conn = Connection(server, LDAP_USER, LDAP_PASSWORD, auto_bind=True)
 
     search_filter = f'(uid={student_id})'
-    conn.search(BASE_DN, search_filter, attributes=['uid', 'cn', 'points'])
+    conn.search(BASE_DN, search_filter, attributes=['uid', 'cn', 'description'])
 
     if conn.entries:
-        # Если пользователь есть - обновим баллы:
+        # Обновляем баллы, если пользователь уже существует
         dn = conn.entries[0].entry_dn
-        conn.modify(dn, {'points': [(MODIFY_REPLACE, [points])]})
+        print(f"Updating user: {dn}")
+        conn.modify(dn, {'description': [(MODIFY_REPLACE, [str(points)])]})
     else:
-        # Если пользователя нет - зарегистрируем и обновим банны:
-        dn = f'uid={student_id},{BASE_DN}'
-        conn.add(dn, ['inetOrgPerson'], {'uid': student_id, 'cn': full_name, 'points': points})
+        # Если нет пользователя - создаем нового
+        dn = f'uid={student_id},ou=People,{BASE_DN}'
+        print(f"Creating user: {dn}")
+
+
+        if isinstance(full_name, str):
+            sn = full_name.split()[1] if len(full_name.split()) > 1 else full_name
+        else:
+            sn = "Unknown"
+
+        conn.add(dn, ['inetOrgPerson'], {'uid': student_id, 'cn': full_name, 'sn': sn, 'description': str(points)})
 
     conn.unbind()
 
